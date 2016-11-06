f5c82b6efc42cbd74c6195c518c2cbe401e1fbf8
louis <louis@DESKTOP-IREMMTV>
2016-11-05 20:38:49 -0400

    update des packages

---
+++ VisuaLing/src/gui/Interface_choix_modeController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.IOException;
+import javafx.scene.Parent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.control.Button ; 
+import javafx.scene.control.TextArea;
+
+
+
+/**
+ * FXML Controller class
+ *
+ * @author louis
+ */
+public class Interface_choix_modeController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * 
+     */
+    //Accéder a l'interface de création de sport temps réel
+    
+    @FXML private Button boutonTempsReel ;    @FXML private Button boutonImageParImage ;
+    @FXML private Button boutonannuler; 
+   
+    
+    
+    
+    @FXML 
+    public void bouton_annuler (ActionEvent event) throws IOException {
+        Parent interface_acceuil_Parent = FXMLLoader.load (getClass().getResource("Interface_accueil.fxml"));
+        Scene interface_acceuil_scene = new Scene (interface_acceuil_Parent); 
+        Stage window = (Stage) boutonannuler.getScene().getWindow();
+        window.setScene(interface_acceuil_scene);
+        window.show();
+        
+    }
+    @FXML
+     public void bouton_TempsReelAction (ActionEvent event)throws IOException {
+        Parent tempsreel_parent = FXMLLoader.load (getClass().getResource("Interface_creer_sport_TR.fxml"));
+        Scene tempsreel_scene = new Scene (tempsreel_parent); 
+        Stage window = (Stage) boutonTempsReel.getScene().getWindow();
+        window.setScene(tempsreel_scene);
+        window.show();
+     }      //Accéder  a l'interface de création de sport image par image
+      @FXML
+        public void bouton_ImageParImageAction (ActionEvent event)throws IOException {
+        Parent imageparimage_parent = FXMLLoader.load (getClass().getResource("Interface_creer_sport_IPI.fxml"));
+        Scene imageparimage_scene = new Scene (imageparimage_parent); 
+        Stage window = (Stage) boutonImageParImage.getScene().getWindow();
+        window.setScene(imageparimage_scene);
+        window.show();
+      }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ VisuaLing/src/gui/ModifObstacleController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author antoinehudon
+ */
+public class ModifObstacleController implements Initializable {
+
+    @FXML private Button boutonAnnuler;
+    @FXML private Button boutonParcourir;
+    @FXML private Button boutonOk;
+    @FXML private ImageView ivImage;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    @FXML 
+    public void boutonOkAction(ActionEvent event) throws MalformedURLException{
+        Stage stage = (Stage) boutonOk.getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    public void boutonAnnulerAction(ActionEvent event) {
+          Stage stage = (Stage) boutonAnnuler.getScene().getWindow();
+          stage.close();
+    }  
+    
+    @FXML 
+    public void boutonParcourirAction(ActionEvent event) throws MalformedURLException{
+         Stage window = (Stage) boutonParcourir.getScene().getWindow();
+         FileChooser fileChooser = new FileChooser();
+         fileChooser.setTitle("Choisir une image");
+         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Fichier image", "*.png", "*.jpg"));
+         File selectedFile = fileChooser.showOpenDialog(window);
+         String path = selectedFile.getPath();
+         File imageFile = new File(path);
+            if (imageFile.exists()) {
+                String imagepath = imageFile.toURI().toURL().toString();
+                Image image = new Image(imagepath);
+                ivImage.setImage(image);
+         }
+    }
+    
+
+    
+}
