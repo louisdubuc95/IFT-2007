f8c8c2a7d68f0173badbed4dd12cbafb56563767
stlau <stlau@DESKTOP-VC4VUTU>
2016-11-06 12:14:18 -0500

    bouton charger et ouvrir

--- VisuaLing/src/gui/Interface_CreerObjectifController.java
+++ VisuaLing/src/gui/Interface_CreerObjectifController.java
@@ -7,21 +7,36 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author stlau
  */
-public class Interface_CreerObstacleController implements Initializable {
+public class Interface_CreerObjectifController implements Initializable {
+    
+    @FXML
+    private Button boutonAnnuler;
 
+    
+    @FXML
+    private void boutonQuitterAction(ActionEvent event) {
+        Stage window = (Stage) boutonAnnuler.getScene().getWindow();
+        window.close();
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+   
     
 }
